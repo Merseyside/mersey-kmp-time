dacd04eefc7288541c66240a221c3ad023a03345
Vladimir <v.mudryakov@dev.izibook.ru>
2022-04-04 20:00:01 +0300

    review

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 
-    val catalogVersions = "1.3.9"
+    val catalogVersions = "1.4.0"
     val group = "io.github.merseyside"
     versionCatalogs {
         val multiplatformLibs by creating {
