eac697be24a4ed5011d16448a04901dca0c65422
Merseyside <ivanklessablin@gmail.com>
2025-05-19 14:33:01 +0500

    Small change

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Application {
     const val applicationId = "com.merseyside.merseyLib"
 
-    const val compileSdk = 34
-    const val targetSdk = 34
+    const val compileSdk = 35
+    const val targetSdk = 35
     const val minSdk = 23
 }
